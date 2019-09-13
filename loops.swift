8e33a4a73ba37a7ce86248381859d6f288b142dc
Americo_IDS <ameneses@ids.com.mx>
2019-09-13 11:03:42 -0500

    loos 1

---
+++ loops.swift
@@ -0,0 +1,20 @@
+var str = "Hello Ame";
+var sum = 0;
+var num = 5;
+/* for _ in 1...5{
+    print(str)
+    
+} 
+for counter in 1...9{
+    sum += counter
+}
+
+while num > 0 {
+    print(num)
+    num -= 1
+}*/
+
+repeat {
+    print("Hello Master")
+    num -= 1
+} while num > 0
