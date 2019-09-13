4592a276759129a23aeb8b3ee985b6faed2a04a1
Americo_IDS <ameneses@ids.com.mx>
2019-09-13 09:43:34 -0500

    else if conditions

---
+++ elseIf.swift
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+if( (a < 8 && c > 50) || b == 20){
+    print("condition 1");
+}else if (b > 31) {
+    print("Condition 2");
+}else if (c < 2) {
+    print("contition 3");
+}else{
+    print("condition 4");
+}
