281f45b31e9c29f1e9a0a2ee49bee99c1e0dadd0
Americo_IDS <ameneses@ids.com.mx>
2019-09-13 11:32:00 -0500

    cambiando definicion de params

--- functions.swift
+++ functions.swift
@@ -1,8 +1,8 @@
-func sumTwoNumbers(arg param1:Int,arg param2:Int) -> Int {
+func sumTwoNumbers(using param1:Int,and param2:Int) -> Int {
     let a = param1;
     let b = param2;
     let res = a + b;
     return res;
 }
-let sum = sumTwoNumbers(arg: 5, arg: 3)
+let sum = sumTwoNumbers(using: 5, and: 4)
 print(sum)
