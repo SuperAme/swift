9d7eb3a518a0abe69569412f60b03d814e8ba748
Americo_IDS <ameneses@ids.com.mx>
2019-09-13 11:26:37 -0500

    functions

---
+++ functions.swift
@@ -0,0 +1,8 @@
+func sumTwoNumbers(arg param1:Int,arg param2:Int) -> Int {
+    let a = param1;
+    let b = param2;
+    let res = a + b;
+    return res;
+}
+let sum = sumTwoNumbers(arg: 5, arg: 3)
+print(sum)
